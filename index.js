4920ba29a3da6cd20cb4dac4051408251bd81e2d
Zach Pardee <zachpardee@gmail.com>
2019-04-29 09:40:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+    drivers.forEach(driver => {
+        console.log(driver.name);
+    });
+}
+
+function logDriversByHometown(drivers, location){
+    drivers.forEach(driver => {
+        if (driver.hometown === location)
+        console.log(driver.name);
+    });
+}
+
+const driversByRevenue = function (drivers) {
+    return drivers.slice().sort(function (driverOne, driverTwo) {
+      return driverOne.revenue - driverTwo.revenue;
+    });
+  };
+
+  const driversByName = function (drivers) {
+    return drivers.slice().sort(function (driverOne, driverTwo) {
+      return driverOne.name.localeCompare(driverTwo.name);
+    });
+  };
+  
+  const totalRevenue = function (drivers) {
+    return drivers.reduce(function (total, currentDriver) {
+      return currentDriver.revenue + total;
+    }, 0);
+  };
+  
+  const averageRevenue = function (drivers) {
+    return totalRevenue(drivers) / drivers.length;
+  };
